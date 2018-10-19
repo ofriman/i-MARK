adbda676afc106e1ad44e719fcef4d41de04920c
Friman <ofriman@ebay.com>
2018-10-19 11:45:47 +0300

    refactor playground

--- playground.js
+++ playground.js
@@ -1,21 +1,17 @@
-console.log("n=" + process.argv[2]);
-console.log("i-Mark({" + process.argv[3] + "}, {"+ process.argv[4] +"})");
-
 const number = +process.argv[2];
-const s = process.argv[3].split(",").map(s => +s);
-const d = process.argv[4].split(",").map(d => +d);
-const filename = process.argv[5];
-const preIndexFilter = +process.argv[6] || 0;
-const grundyFilter = +process.argv[7] || 0;
-
-const table = require("./imark.js").calc(number, s, d);
-
-if (filename) {
-	if (filename.includes(".csv")) {
-		const rowFormat = array => array.join(",") + "\n";
-		const writeStream =  require('fs').createWriteStream(filename);
-		writeStream.write(table.reduce((acc, value, index) => (acc + rowFormat([index, value])), rowFormat(["n", "g(K n)"])));
-		writeStream.close();
-		console.log(filename + " saved!");
-	}
-}
+const s = process.argv[3];
+const d = process.argv[4];
+const table = require("./imark.js")
+  .calc(number,
+    s.split(",").map(s => +s),
+    d.split(",").map(d => +d)
+  );
+const createCsvWriter = require('csv-writer').createArrayCsvWriter;
+const csvWriter = createCsvWriter({
+  header: ['n', 'g(n)'],
+  path: `i-MARK${s}_${d}.csv`
+});
+csvWriter.writeRecords(table.map((grundy, index) => [index, grundy]))
+  .then(() => {
+    console.log('saved!');
+  });
