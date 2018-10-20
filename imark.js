c2ee8ab2c68b7a514463aa31e7179be5b5aadcef
Friman <ofriman@ebay.com>
2018-10-20 09:25:26 +0300

    refactor imark

--- imark.js
+++ imark.js
@@ -1,18 +1,19 @@
+const union = require('lodash.union');
+
 const mex = array => array
 	.sort((a, b) => a - b)
 	.reduce((mex, value) => mex < value ? mex : (value + 1) ,0);
 
-const options = (n,s, d) => s.filter(s_num => n >= s_num).map(s_num => n - s_num)
-	.concat(d.filter(d_num => n && n % d_num === 0).map(d_num => n / d_num))
-	.filter((item, pos, a) => a.indexOf(item) == pos);
+const options = (n,s, d) => union(
+	s.filter(s_num => n >= s_num).map(s_num => n - s_num),
+	d.filter(d_num => n && n % d_num === 0).map(d_num => n / d_num)
+);
 
 const calc = (n, s, d) => {
   const table = [];
-
   for (let i = table.length; i <= n; i += 1) {
     table[i] = mex(options(i, s, d).map(number => table[number]));
   }
-
   return table;
 };
 
